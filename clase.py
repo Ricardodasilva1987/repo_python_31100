5697a6e632819a063842bf2eeac6dfb36abca1fd
Ricardo Da Silva <dasilvaricardog@gmail.com>
2022-08-09 21:29:52 -0400

    segun commit de mi clase

--- clase.py
+++ clase.py
@@ -1,6 +1,7 @@
 class Persona:
-    def __init__(self, nombre, apellido): 
+    def __init__(self, nombre, apellido,dni): 
         self.nombre =nombre
         self.apellido =apellido
+        self.dni=dni
     def hablar(self):
         print(f"Hola soy {self.nombre}")
